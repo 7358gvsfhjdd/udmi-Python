5100b678c48b45bd48af48cd3540dade63916399
Sanmati Manoj Gosavi <89386983+7358gvsfhjdd@users.noreply.github.com>
2026-01-22 23:09:45 +0530

    Implement Animal and Dog classes with basic functionality

---
+++ 16_oops/04_inheritance.py
@@ -0,0 +1,12 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name 
+    def speak(self):
+        print("Generic animal sound")
+    
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")    
+        
+a = Animal("Dog")
+a.speak()
