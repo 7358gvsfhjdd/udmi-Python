88cdd05f84d3e697975f8d20fa26659d234bee70
Sanmati Manoj Gosavi <89386983+7358gvsfhjdd@users.noreply.github.com>
2026-02-03 11:37:09 +0530

    Implementing Walrus Function

---
+++ 17_Advance python concept/12_walrus.py
@@ -0,0 +1,17 @@
+def very_slow_func():
+    print("Something....")
+    print("Something....")
+    print("Something....")
+    print("something....")
+    print("Something....")
+    return 70
+
+#a = very_slow_func()
+#if (a>10):
+#    print(a)
+
+if ((a:=very_slow_func())>10):
+    print(a)    
+          
+else:
+    print("Its not greater than 10")
