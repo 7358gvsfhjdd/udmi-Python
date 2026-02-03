916b235cd7d3e811ac081592e1372affaec08976
Sanmati Manoj Gosavi <89386983+7358gvsfhjdd@users.noreply.github.com>
2026-02-03 16:28:01 +0530

    implementing walrus

---
+++ 17_Advance python concept/12_walrust.py
@@ -0,0 +1,12 @@
+def very_slow_func():
+    print("Something....")
+    print("Something....")
+    print("Something....")
+    print("something....")
+    print("Something....")
+    return 70
+
+while(data:=input("Enter the value")):
+    print(data)
+    if data =="q":
+        break
