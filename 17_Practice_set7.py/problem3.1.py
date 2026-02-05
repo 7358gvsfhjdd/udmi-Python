388abc9fc7b25a578d6392cc0a12f9f4577004af
Sanmati Manoj Gosavi <89386983+7358gvsfhjdd@users.noreply.github.com>
2026-02-05 18:11:04 +0530

    Static methon and class method

---
+++ 17_Practice_set7.py/problem3.1.py
@@ -0,0 +1,14 @@
+class Mathutils:
+    def __init__(self):
+        pass
+    @staticmethod
+    def add(a, b):
+        return a + b
+    
+    @classmethod
+    def description(cls):
+        print("this is a utility class for math operations. ")
+        
+a = Mathutils
+print(a.add(5,34))
+a.description()
