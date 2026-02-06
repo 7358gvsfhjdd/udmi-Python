4d06c08a80cc8f4341c9df804b428663d98b8e31
Sanmati Manoj Gosavi <89386983+7358gvsfhjdd@users.noreply.github.com>
2026-02-06 11:03:00 +0530

    Try and except solution

---
+++ 17_Practice_set7.py/problem5.1.py
@@ -0,0 +1,20 @@
+class NegativeNumberError(Exception):
+    pass
+
+try:
+    num = int(input("Enter a number: "))
+    
+    if num<0:
+        raise NegativeNumberError("Number is negative")
+    
+    result = 45/num
+    print(f"The result is {result}")
+    
+except ValueError:
+    print("Error: Please enter a proper number")
+    
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero")
+    
+except NegativeNumberError as e:
+    print(f"Error: The number cannot be negative", e)
