5cec1fef314eef208e0cbf33e4d31fe4b49f3082
Sanmati Manoj Gosavi <89386983+7358gvsfhjdd@users.noreply.github.com>
2026-02-09 19:17:19 +0530

    with f open

--- 18_file/05_with.py
+++ 18_file/05_with.py
@@ -1,7 +1,8 @@
-f = open("18_file/sam.txt", "r")
-
-content = f.read()
-
-print(content)
-
-f.close()
+#f = open("18_file/sam.txt", "r")
+#content = f.read()
+#print(content)
+#f.close()
+with open("18_file/sam.txt", "r") as f:
+    content = f.read()
+    print(content)
+    #no need to write f.close(because file is alredy chosenby default)
