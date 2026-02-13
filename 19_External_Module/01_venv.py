fd98a34369cc647fb802f04022931000fedaa45e
Sanmati Manoj Gosavi <89386983+7358gvsfhjdd@users.noreply.github.com>
2026-02-13 19:55:10 +0530

    lecktures

---
+++ 19_External_Module/01_venv.py
@@ -0,0 +1,8 @@
+# -m venv env1
+#.\env1\Scripts\Activate.ps1
+#.\env1\Scripts\Activate.ps1
+# that all install seperatly in env 1 envirment thats before installed into the global envirnment
+#pip install --upgrade requests is use for install out dated package
+#pip install -r .\requirments.txt to activate env2
+#pip install -r requirements.txt (install in env2) 
+import moviepy
