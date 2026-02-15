ff0b48a3811b1eaf223c4d3946d2eaf03bf631c7
Sanmati Manoj Gosavi <89386983+7358gvsfhjdd@users.noreply.github.com>
2026-02-15 11:20:32 +0530

    regular expression

---
+++ 19_External_Module/03_eregular_ex.py
@@ -0,0 +1,11 @@
+import re
+text = "The quick brown focx jum over the lazy  brown"
+
+#Search for pattern 
+
+match = re.search("brown", text)
+print(match)
+if match:
+    print("Match found!")
+    print("Start index:", match.start())
+    print("End index:", match.end())
